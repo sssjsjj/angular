e80290a0daa8defa04d3935cfb3c1c7042e42d5b
sssjsjj <sssjsjj@naver.com>
2019-01-07 10:01:19 +0900

    app routing conflickt commint

--- study/src/app/app-routing/app-routing.module.ts
+++ study/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Study1Component } from '../study1/study1.component';
-import { Study2Component } from '../study2/study2.component';
 import { HomeComponent } from '../home/home.component';
 import { ExObservableComponent } from '../ex-observable/ex-observable.component';
 import { Study1n2Component } from '../study1n2/study1n2.component';
 import { Observable1Component } from '../observable1/observable1.component';
 import { Observable2Component } from '../observable2/observable2.component';
+
 const routes: Routes = [
   {
     path: 'study1-1',
@@ -26,17 +26,13 @@ const routes: Routes = [
     component: Study1n2Component,
   },
   {
-    path: 'study2',
-    component: Study2Component
-  },
-  {
     path: 'observable1',
     component: Observable1Component
   },
   {
     path: 'observable2',
     component: Observable2Component
-]
+  },
   {
     path: '',
     component: HomeComponent
@@ -46,7 +42,7 @@ const routes: Routes = [
     redirectTo: '',
     pathMatch: 'full'
   },
-]; 
+];
 @NgModule({
   declarations: [],
   imports: [
